e413735fd5a9b83a1113b9ead44968b0075acab4
Ali Erkin Avcı <alierkinavci20@hotmail.com>
2022-04-11 06:26:08 +0300

    sign up eklendi ve son kullanıcı kaydedildi

--- form_LogIn.cs
+++ form_LogIn.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
 
 namespace OOP2Lab1
 {
     public partial class form_LogIn : Form
     {
         private form_MainGame mainGameForm;
+        private form_SignUp SignUpForm;
+
         public form_LogIn()
         {
             InitializeComponent();
@@ -25,11 +29,29 @@ namespace OOP2Lab1
                 mainGameForm = new form_MainGame();
                 mainGameForm.ShowDialog();
                 this.Hide();
+                ///////////////////
+                string path = System.IO.Directory.GetCurrentDirectory() + "\\lastuser.txt";
+                if (!File.Exists(path))  // If the file doesn't exist, the folder will create in this if statement.
+                {
+                    File.Create(path);
+                }
+                File.WriteAllText(path, textBox_Username.Text);
+                /////////////////////////
             }
             else if(textBox_Username.Text == "admin" && textBox_Password.Text == "admin")
             {
+                ////////////////////////
+                string path = System.IO.Directory.GetCurrentDirectory() + "\\lastuser.txt";
+                if (!File.Exists(path))  // If the file doesn't exist, the folder will create in this if statement.
+                {
+                    File.Create(path);
+                    File.WriteAllText(path, textBox_Username.Text);
+                }
+                File.WriteAllText(path, textBox_Username.Text);
+                //////////////////////
                 mainGameForm = new form_MainGame();
                 mainGameForm.ShowDialog();
+               
                 
             }
             else
@@ -39,10 +61,33 @@ namespace OOP2Lab1
 
         private void textBox_Username_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))////prevents user to press numbers in username textbox
             {
                 e.Handled = true;
             }
+        }
+
+        private void textBox_Username_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_SıgnIn_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void form_LogIn_Load(object sender, EventArgs e)
+        {
+            string path = System.IO.Directory.GetCurrentDirectory() + "\\lastuser.txt";
+            var str =File.ReadAllText(path);
+            textBox_Username.Text = str;
+        }
+
+        private void button_SignUp_Click(object sender, EventArgs e)
+        {
+            SignUpForm = new form_SignUp();
+            SignUpForm.ShowDialog();
         }
     }
 }
---
+++ form_SignUp.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+
+namespace OOP2Lab1
+{
+    
+    public partial class form_SignUp : Form
+    {
+
+       
+        public form_SignUp()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void form_SignIn_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+
+      
+        
+        private void button_CreateUser_Click(object sender, EventArgs e)
+        {
+            
+            string path = System.IO.Directory.GetCurrentDirectory() + "\\user.txt";
+            if (!File.Exists(path))  // If the file doesn't exist, the folder will create in this if statement.
+            {
+                StreamWriter writer1 = new StreamWriter(path); 
+                writer1.Close();
+            }
+            
+            StreamWriter writer = new StreamWriter(path);
+            writer.WriteLine(textBox_Username.Text);
+            writer.WriteLine(textBox_Password.Text);
+            writer.WriteLine(textBox_NameSurname.Text);
+            writer.WriteLine(textBox_PhoneNumber.Text);
+            writer.WriteLine(textBox_Adress.Text);
+            writer.WriteLine(textBox_City.Text);
+            writer.WriteLine(textBox_Country.Text);
+            writer.WriteLine(textBox_Email.Text);
+            writer.WriteLine(".");
+            writer.Close();
+           
+
+        }
+    }
+}
